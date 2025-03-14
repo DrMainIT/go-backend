d77cc4aaefe25becc018be1e24df6e28ce54b776
francesco z <drmainit@gmail.com>
2025-03-14 21:38:17 +0100

    admin page

--- main.go
+++ main.go
@@ -2,18 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
+
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/template/html/v2"
 	"github.com/joho/godotenv"
-	"gorm.io/gorm"
 	"gorm.io/driver/postgres"
-	"os"
+	"gorm.io/gorm"
 )
 
-type Spesa struct {
+type Urls struct {
 	gorm.Model
-	Name string
+	Name     string
+	approved bool
 }
+
 func Config(key string) string {
 	err := godotenv.Load(".env")
 	if err != nil {
@@ -23,45 +26,64 @@ func Config(key string) string {
 
 }
 
-func queryName(db *gorm.DB) []string {
-	var products []Spesa 
-	result := db.Find(&products)
+func queryName(db *gorm.DB,approved bool) []string {
+	var urls []Urls
+	result := db.Find(&urls)
 	if result.Error != nil {
 		return nil
 	}
-	names := []string{}
-	for _,product := range products {
-		names = append(names,product.Name)
+	links := []string{}
+	for _, url := range urls {
+		if url.approved == approved {
+		links = append(links, url.Name)
+		}
 	}
-	return names
+	return links
 }
 
 func main() {
-	engine := html.New("./views",".html")
+	engine := html.New("./views", ".html")
 	app := fiber.New(fiber.Config{Views: engine})
 	err := godotenv.Load(".env")
 	if err != nil {
 		fmt.Print("Error loading .env file")
 	}
-	db,err := gorm.Open(postgres.Open(Config("POSTGRES_URL")),&gorm.Config{})
-	db.AutoMigrate(&Spesa{})
+	db, err := gorm.Open(postgres.Open(Config("POSTGRES_URL")), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.AutoMigrate(&Urls{})
 	app.Get("/", func(c fiber.Ctx) error {
 		// list all elements
-		names := queryName(db)
-
-		return c.Render("index",fiber.Map{
-			"ingredients": names,
-			"Title": "Articles to buy",
-		})
+		return c.Render("index", fiber.Map{})
 	})
 	app.Get("/add", func(c fiber.Ctx) error {
 		// get values from the form
-		product := c.FormValue("product")
-		db.Create(&Spesa{Name: product})
-		names := queryName(db)
-		return c.Render("index",fiber.Map{
-			"ingredients": names,
-			"Title": "Articles to buy",
+		url := c.FormValue("url")
+
+		db.Create(&Urls{Name: url, approved: false})
+		//names := queryName(db)
+		return c.Render("index", fiber.Map{})
+	})
+
+	app.Get("/login", func(c fiber.Ctx) error {
+		return c.Render("login", fiber.Map{})
+	})
+	app.Post("/login", func(c fiber.Ctx) error {
+		username := c.FormValue("username")
+		password := c.FormValue("password")
+		if os.Getenv("USERNAME") == username && os.Getenv("PASSWORD") == password {
+			return c.Redirect().To("/admin")
+		}
+		return c.Redirect().To("/login")
+	})
+
+	app.Get("/admin", func(c fiber.Ctx) error {
+		names := queryName(db,false)
+
+		return c.Render("index", fiber.Map{
+			"urls": names,
+			"Title":       "Articles to buy",
 		})
 	})
 	app.Listen(":3000")
