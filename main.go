55f26cf6afe2ff5e4e1bc4fba2622516f7abca77
francesco z <drmainit@gmail.com>
2025-03-16 01:21:23 +0100

    finished backend

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
-
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/template/html/v2"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"  
+    "github.com/gofiber/fiber/v3/middleware/cors"
+	"github.com/gofiber/fiber/v3/middleware/logger"
+    "github.com/gofiber/fiber/v3/middleware/encryptcookie"
 )
 
 type Urls struct {
@@ -44,6 +46,19 @@ func queryName(db *gorm.DB,approved bool) []string {
 func main() {
 	engine := html.New("./views", ".html")
 	app := fiber.New(fiber.Config{Views: engine})
+	// Cookie 
+	app.Use(encryptcookie.New(encryptcookie.Config{
+		Key: "secret-thirty-2-character-string",
+	}))
+	// Initialize default config
+	app.Use(cors.New())
+	
+	app.Use(logger.New())
+	// Or extend your config for customization
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"https://gofiber.io", "https://gofiber.net",Config("FRONTEND_URL")},
+		AllowHeaders: []string{"Origin", "Content-Type", "Accept"},
+	}))
 	err := godotenv.Load(".env")
 	if err != nil {
 		fmt.Print("Error loading .env file")
@@ -57,52 +72,32 @@ func main() {
 		// list all elements
 		urls := queryName(db,true)
 		return c.JSON(fiber.Map{
-			"Title": "dake",
 			"urls":  urls,
 		})
-		// return c.Render("index", fiber.Map{
-		//	"Title": "dake",
-		//	"urls": urls,
-		//})
 	})
-	app.Get("/add", func(c fiber.Ctx) error {
-		// get values from the form
+	app.Post("/add", func(c fiber.Ctx) error {
+		// get values from json
 		url := c.FormValue("url")
 
 		db.Create(&Urls{Name: url,Approved: false})
-		return nil
+		return c.Redirect().To(Config("FRONTEND_URL")+"/thankyou")
 	
-	})
-
-	app.Get("/login", func(c fiber.Ctx) error {
-		return c.Render("login", fiber.Map{})
-	})
-	app.Post("/login", func(c fiber.Ctx) error {
-		username := c.FormValue("username")
-		password := c.FormValue("password")
-		if os.Getenv("USERNAME") == username && os.Getenv("PASSWORD") == password {
-			return c.Redirect().To("/admin")
-		}
-		return c.Redirect().To("/login")
 	})
 
 	app.Get("/admin", func(c fiber.Ctx) error {
 		names := queryName(db,false)
-
-		return c.Render("admin", fiber.Map{
+		return c.JSON(fiber.Map{
 			"urls": names,
-			"Title":       "Articles to buy",
 		})
 	})
 	app.Post("/admin/accept", func(c fiber.Ctx) error{
-		names := queryName(db,false)
-		url := c.FormValue("link")
-		fmt.Print(url)
-		db.Model(&Urls{}).Where("Name = ?", url).Update("approved", true)
-		return c.Render("admin", fiber.Map{
-			"urls": names,
-			"Title": "Articles to buy",
-		})
+		url := c.FormValue("url")
+		password := c.FormValue("password")
+
+		if password == os.Getenv("PASSWORD") {
+			db.Model(&Urls{}).Where("Name = ?", url).Update("approved", true)
+		}
+		return c.Redirect().To(Config("FRONTEND_URL")+ "/admin")
 	})
 	
 	app.Listen(":3000")
